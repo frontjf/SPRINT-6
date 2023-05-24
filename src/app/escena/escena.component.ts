87c8a8340529afba5b33118e7cd2513cc19015db
frontjf <frontjf@gmail.com>
2023-05-24 23:16:07 +0200

    Ejercicio 2 Sprint 6

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'; // Importa los módulos Component y Input del paquete @angular/core, 
+                                                  // son necesarios para definir componentes y usar la directiva @Input.
+
+interface Frase {   //declara una interfaz llamada Frase que define la estructura de un objeto
+  texto: string;    // que tiene una propiedad texto de tipo string.
+  
+}
 
 @Component({
   selector: 'app-escena',
-  template: `
-  <p>"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p>`,
+  templateUrl: './escena.component.html',
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
 
+  @Input() frases: Frase[];   //  Es una anotación @Input que define la propiedad frases del componente como una entrada.
+                              // Esto significa que el componente puede recibir un array de objetos Frase desde su componente padre.
+
+  constructor() {            // Es el constructor de la clase EscenaComponent. En este caso, inicializa la propiedad frases
+    this.frases = [];        // como un array vacío. Esto garantiza que la propiedad esté definida y lista para recibir valores. 
+    
+
+}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+
+interface Frase {     // estructura con el componente texto del tipo string
+  texto: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +12,13 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  frases: Frase[] = [     // frases con la estructura de Frase para poner texto
+    { texto: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacia" },
+    { texto: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes." },
+    { texto: "L'heroi va decidir travessar la porta que el portava a casa" },
+    { texto: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..." }
+  ];   
+  
+
 }
+
