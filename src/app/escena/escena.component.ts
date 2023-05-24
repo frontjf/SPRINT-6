f3335f17ce478eb04b30a6d89cf715ee4752a6e2
frontjf <frontjf@gmail.com>
2023-05-24 20:10:15 +0200

    Hasta ejercicio 1 Sprint 6

---
+++ src/app/escena/escena.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escena',
+  template: `
+  <p>"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p>`,
+  styleUrls: ['./escena.component.css']
+})
+export class EscenaComponent {
+
+}
