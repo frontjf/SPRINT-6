0b9ff4e635a9f6211371acb1d27329f6751b3e79
frontjf <frontjf@gmail.com>
2023-05-26 03:29:54 +0200

    Nivel 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SPRINT-6';
+  mostrarEscena(){}
 }
+
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 
 interface Frase {     // estructura con el componente texto del tipo string
@@ -12,13 +12,25 @@ interface Frase {     // estructura con el componente texto del tipo string
 })
 export class HomeComponent {
 
+  mostrarEscena: boolean = false;                     // inicializamos mostrarEscena en falso
+  @Output() iniciarJuego = new EventEmitter<void>();  // @Output es un decorador para definir la salida de la propiedad iniciarJuego 
+  // esta instacia Eventemitter emite (o envia) el evento iniciar juego
+  // vamos que output dice que iniciarjuego esta listo para salir y EventEmitter es quien lo lleva donde sea
+  // lo de void es pq ira vacio, no se esperan valores, no llevara informacion sino una accion que hay que ejecutar
+
   frases: Frase[] = [     // frases con la estructura de Frase para poner texto
-    { texto: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacia" },
-    { texto: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes." },
-    { texto: "L'heroi va decidir travessar la porta que el portava a casa" },
-    { texto: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..." }
+    { texto: "Nuestro heroe navegaba por el espacio cuando recibio una llamada de alerta de una nave que habia a lo lejos..." },
+    { texto: "Habian 2 puertas con la letra A dibujada, y encima un cartel que decia..." },
+    { texto: "En una puerta hay Aliens carnivoros muertos de hambre, y en la otra esta Fernando Herrera dando clases de Angular las 24 horas del dia... " },
+    { texto: "Nuestro heroe eligio una puerta y tuvo la mala suerte de acabar en las clases de Angular..." }
   ];   
+
   
+
+  empezarJuego() {                  // metodo empezarJuego 
+    this.mostrarEscena = true;      // pasamos mostrarEscena a true
+    this.iniciarJuego.emit();       // emit es un metodo que 'emite' el evento iniciarJuego (lo 'escucharan' en app.component.html)
+  }
 
 }
 
